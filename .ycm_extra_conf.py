4c6e41faf7481bb207c6aa11b29e8d439a7c3557
Scott Olson <scott@scott-olson.org>
2014-06-02 22:57:17 -0400

    Add hack to fix YouCompleteMe (libclang include directory nonsense).

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -7,6 +7,21 @@ def FlagsForFile(filename, **kwargs):
     # Force YCM to recognize all files as C++, including header files
     flags.extend(['-x', 'c++'])
 
+    # Disgusting hack to get around libclang's failure to use the correct
+    # lookup paths. These include directories were obtained from the command
+    # `echo | clang -v -E -x c++ -`
+    sys_includes = """
+    /usr/bin/../lib64/gcc/x86_64-unknown-linux-gnu/4.9.0/../../../../include/c++/4.9.0
+    /usr/bin/../lib64/gcc/x86_64-unknown-linux-gnu/4.9.0/../../../../include/c++/4.9.0/x86_64-unknown-linux-gnu
+    /usr/bin/../lib64/gcc/x86_64-unknown-linux-gnu/4.9.0/../../../../include/c++/4.9.0/backward
+    /usr/local/include
+    /usr/bin/../lib/clang/3.4.1/include
+    /usr/include
+    """.split()
+
+    for include in sys_includes:
+        flags.extend(['-isystem', include])
+
     return {
         'flags': flags,
         'do_cache': True
