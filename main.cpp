6ee4872f232c802187a0e459e822cb70976ebf85
Scott Olson <scott@scott-olson.org>
2014-06-02 22:59:55 -0400

    Add simple RAII wrapper for libedit/editline.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,37 @@ extern "C" {
 
 using namespace llvm;
 
+class EL {
+ public:
+  using PromptFnType = const char* (*)(EditLine*);
+
+  explicit EL(const char* program_name);
+  ~EL();
+
+  void setPromptFn(PromptFnType prompt_fn);
+  std::string getLine();
+ private:
+  EditLine* el;
+};
+
+EL::EL(const char* program_name) {
+  el = el_init(program_name, stdin, stdout, stderr);
+}
+
+EL::~EL() {
+  el_end(el);
+}
+
+void EL::setPromptFn(EL::PromptFnType promptFn) {
+  el_set(el, EL_PROMPT, promptFn);
+}
+
+std::string EL::getLine() {
+  int len;
+  const char* rawLine = el_gets(el, &len);
+  return std::string(rawLine, len);
+}
+
 cl::opt<int> Num(cl::Positional, cl::desc("<number>"), cl::init(3));
 
 int main(int argc, char** argv) {
@@ -16,14 +47,11 @@ int main(int argc, char** argv) {
     std::cout << "What's going on?\n";
   }
 
-  EditLine* el = el_init(argv[0], stdin, stdout, stderr);
-  el_set(el, EL_PROMPT, (const char*(*)(EditLine*)) [](EditLine*){ return "> "; });
+  EL el(argv[0]);
+  el.setPromptFn([](EditLine*){ return "> "; });
 
   while (true) {
-    int count;
-    const char* raw_line = el_gets(el, &count);
-
-    std::string line(raw_line, count);
+    std::string line = el.getLine();
     std::cout << line;
   }
 
